3cad9eacfbb22cabe889677c93aeafa14579fff0
TIm <barboza-82@mail.ru>
2023-01-11 21:56:32 +0300

    created db-connection

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+  error_reporting(0);
+  $conn = mysqli_connect('localhost', 'root', '', 'movies_api');
+
+  if(!$conn){
+    echo '{"message":"Unable to connect to database"}';
+    die();
+  }else{
+    echo "connected successfully";
+  }
+
+?>
